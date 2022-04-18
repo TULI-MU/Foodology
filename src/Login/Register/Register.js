0d2aadb1330930288f4fdb453414ce86f1b261a5
TULI-MU <pauljoyosree12@gmail.com>
2022-04-18 20:51:31 +0600

    register added

--- src/Login/Register/Register.js
+++ src/Login/Register/Register.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import './Register.css';
+import auth from '../../../firebase.init';
 
 const Register = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
+    const navigate = useNavigate();
+
+    const navigateLogin = () => {
+        navigate('/login');
+    }
+    if (user) {
+        navigate('/home');
+    }
+    const handleRegister = e => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+
+        createUserWithEmailAndPassword(email, password);
+    }
     return (
-        <div>
-            <h2>Please Register</h2>
+        <div className='register-form'>
+            <h2 style={{ textAlign: 'center' }}>Please Register</h2>
+            <form onSubmit={handleRegister}>
+                <input type="text" name="name" id="" placeholder='Your Name' />
+                <input type="email" name="email" id="" placeholder='Email Address' required />
+                <input type="password" name="password" id="" placeholder='password' />
+                <input type="submit" value="Register" />
+            </form>
+            <p>Already have an account?<Link to={'/login'} className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
         </div>
     );
 };
